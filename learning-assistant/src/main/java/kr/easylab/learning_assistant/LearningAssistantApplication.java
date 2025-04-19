3144cb934ae91106ec23ae14c840a000eaf224cd
Seunghwa Lee <banksemi@naver.com>
2025-04-20 06:41:50 +0900

    feat: initialize the backend project

---
+++ learning-assistant/src/main/java/kr/easylab/learning_assistant/LearningAssistantApplication.java
@@ -0,0 +1,13 @@
+package kr.easylab.learning_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningAssistantApplication.class, args);
+	}
+
+}
---
+++ learning-assistant/src/test/java/kr/easylab/learning_assistant/LearningAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.easylab.learning_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
